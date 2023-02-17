01024513e9ad3ad0c989bcdf7b2c795b903f05d2
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-17 12:28:31 +0200

    Add a new function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// Perfoming a task
 function greet(name, lastName) {
     console.log('Hello ' + name + ' ' + lastName);
 }
 
-greet('John', 'Smith');
+// Calculating a value
+function square(namber) {
+    return number * number;
+}
+
+
+let number = square(2);
+console.log(number);
