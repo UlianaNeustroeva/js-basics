b9cd08b8fe6dbf51e12f3f0ee52be1a5ad10c63c
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-17 15:12:43 +0200

    Add property

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 function Circle(radius) {
-    console.log('this', this);
     this.radius = radius;
     this.draw = function() {
         console.log('draw');
     }
 }
 
-const Circle1 = new Function('radius', `
-this.radius = radius;
-this.draw = function() {
-    console.log('draw');
+const circle = new Circle(10);
+
+for (let key in circle) {
+    console.log(key, circle[key]);
 }
-`);
-
-const circle = new Circle1(1);
-
-const another = new Circle(1);
