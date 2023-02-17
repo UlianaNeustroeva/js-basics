47476857eab7e3658843cbc092aa353f16f39744
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-17 13:34:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// Perfoming a task
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
-}
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log('draw');
+    }
+};
 
-// Calculating a value
-function square(number) {
-    return number * number;
-}
-
-
-let number = square(2);
-console.log(number);
+circle.draw();
