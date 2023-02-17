3bb6450c54ca3b3319fa37098e3e45e2a8173cfb
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-17 14:12:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// Factory function 
-function createCircle(radius) {
-    return {
-        radius,
-        draw: function() {
-            console.log('draw');
-        }
-    };
-}
-
-const circle = createCircle(1);
-
-// Constructor Function
 function Circle(radius) {
     console.log('this', this);
     this.radius = radius;
@@ -18,5 +5,14 @@ function Circle(radius) {
         console.log('draw');
     }
 }
+
+const Circle1 = new Function('radius', `
+this.radius = radius;
+this.draw = function() {
+    console.log('draw');
+}
+`);
+
+const circle = new Circle1(1);
 
 const another = new Circle(1);
