0c253892116ea9e3e26b6d70b7bd13ab78b44423
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-17 13:51:38 +0200

    Add constructor function

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
+// Factory function 
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
+
+// Constructor Function
+function Circle(radius) {
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function() {
         console.log('draw');
     }
-};
+}
 
-circle.draw();
+const another = new Circle(1);
