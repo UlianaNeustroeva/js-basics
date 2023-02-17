442c2f0fb12173ccbb3e438a0219d8b3e9b616a9
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-17 15:34:49 +0200

    Add methods

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function Circle(radius) {
     this.radius = radius;
+
+this.defaultLocation = { x: 0, y: 0 };
+
+this.computeOptimumLocation = function() {
+    // ...
+}
+
     this.draw = function() {
+        this.computeOptimumLocation();
+
         console.log('draw');
     }
 }
 
 const circle = new Circle(10);
-
-for (let key in circle) {
-    if (typeof circle [key] !== 'function' )
-    console. log (key, circle [key]);
-    }
-    const keys = Object.keys (circle);
-    console. log (keys) ;
-    if ('radius' in circle)
-    console. log ('Circle has a radius.');
+this.computeOptimumLocation();
+circle.draw();
